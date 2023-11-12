fc8b23d83140c9f1a44bf57a360fb5fb1c208659
xopowen <xopowen@gmail.com>
2023-11-12 21:08:31 +0800

    2

--- gulpfile.babel.js/tasks/html.js
+++ gulpfile.babel.js/tasks/html.js
@@ -1,19 +1,19 @@
-const gulp  = require('gulp')
+const gulp  = require('gulp');
 const{src,dest } = gulp
-const fileIndcude = require('gulp-file-include')
-const htmlMin = require('gulp-htmlmin')
-const size = require('gulp-size')
-const plumber = require('gulp-plumber')
-const options = require('../optionsPlagins')
-const webpHtml = require('gulp-webp-html-nosvg')
+const fileIndcude = require('gulp-file-include');
+const htmlMin = require('gulp-htmlmin');
+const size = require('gulp-size');
+const plumber = require('gulp-plumber');
+const options = require('../optionsPlagins');
+const webpHtml = require('gulp-webp-retina-html');
 const html = (assets,build)=>{
-    console.log('work with html'+`:${assets}->${build}`)
+    console.log('work with html'+`:${assets}->${build}`);
     return src(assets)
         .pipe(plumber(options.plumberHTML))
         .pipe(fileIndcude())
         .pipe(size({title:'before minimalist.'}))
-        .pipe(htmlMin(options.htmlMin))
         .pipe(webpHtml())//для вставки конструкции picture вместо img
+        .pipe(htmlMin(options.htmlMin))
         .pipe(size({title:'after minimalist.'}))
         .pipe(dest(build))
 }
