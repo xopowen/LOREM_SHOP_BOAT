0ca09747fa07b625c8621b30580e7a6e217737af
xopowen <xopowen@gmail.com>
2024-05-08 23:11:59 +0800

    6

--- src/js/components/historyOrdersItem.js
+++ src/js/components/historyOrdersItem.js
@@ -1,14 +1,14 @@
 import {addClassForEffect,classNames} from "./constans";
 
-let historyOrdersItems = document.querySelectorAll("."+classNames.historyOrdersItem)
+let historyOrdersItems = document.querySelectorAll("."+classNames.historyOrdersItem);
 
 if(historyOrdersItems){
     historyOrdersItems.forEach(v=>{
         v.addEventListener('click',e=>{
             if(e.target !== v.querySelector('.history-orders__button')){
-                v.classList.toggle(classNames.historyOrdersItem+addClassForEffect.active)
+                v.classList.toggle(classNames.historyOrdersItem+addClassForEffect.active);
             }
-            e.preventDefault()
-        })
-    })
+            e.preventDefault();
+        });
+    });
 }
